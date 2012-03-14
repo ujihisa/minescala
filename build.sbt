e317778608f401e697c914b0dfa8c20b38f884e7
ujihisa <ujihisa@gmail.com>
2012-03-14 00:31:38 -0700

    pure java part works.

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ version := "1.0"
 scalaVersion := "2.9.1"
 
 seq(assemblySettings: _*)
+
+//libraryDependencies += "org.bukkit" %% "bukkit" % "1.2.3-R0.2"
--- src/main/java/Minescala2.java
+++ src/main/java/Minescala2.java
@@ -1,8 +1,14 @@
+package minescala;
 import minescala.Minescala;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Minescala2 {
+public class Minescala2 extends JavaPlugin {
   public static void main(String[] args) {
     System.out.println("from java.");
     Minescala.hello();
+  }
+
+  public void onEnable() {
+    System.out.println("Let's scala!");
   }
 }
